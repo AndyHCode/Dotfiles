a38dcf6ac1c5282ceabbd69dfb5239556f407006
Andy <andyh12965@gmail.com>
2023-03-11 19:02:21 -0500

    Current Config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#set temp an env variable for temp module
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
+DIR="$HOME/.config/polybar"
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+
+
